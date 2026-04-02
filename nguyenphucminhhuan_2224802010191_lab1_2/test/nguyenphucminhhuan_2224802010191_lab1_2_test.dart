819d783df4a34fc887b795f4ebe216d5b9433400
MinhHuan24 <huannguyen.131204@gmail.com>
2026-04-02 14:27:55 +0700

    First commit - Flutter project Lab1

---
+++ nguyenphucminhhuan_2224802010191_lab1_2/test/nguyenphucminhhuan_2224802010191_lab1_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:nguyenphucminhhuan_2224802010191_lab1_2/nguyenphucminhhuan_2224802010191_lab1_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
